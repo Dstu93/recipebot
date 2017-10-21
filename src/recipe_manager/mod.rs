2426c6a056491a41654eb32abef1d5564717dd0d
doenis <d.st@this.com>
2017-10-21 15:02:48 +0200

    added the the util.rs to the recipe_manager/mod.rs

--- src/recipe_manager/mod.rs
+++ src/recipe_manager/mod.rs
@@ -1,5 +1,9 @@
+
+//! this Module is the root of all self written components and functions of this programm
+
 pub mod objects;
 pub mod implementations;
 pub mod services;
 pub mod factory;
 pub mod traits;
+pub mod utils;
