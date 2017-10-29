ba20a0f005078cdd206553959d3ecbed3cb85b75
doenis <d.st@this.com>
2017-10-29 23:58:42 +0100

    ApplicationConfig is now loadable

--- src/recipe_manager/configuration/config_loader.rs
+++ src/recipe_manager/configuration/config_loader.rs
@@ -1,7 +1,7 @@
 
 extern crate serde_json;
 
-use recipe_manager::configuration::config::DatabaseConfig;
+use recipe_manager::configuration::config::*;
 
 use std::io::Error;
 use std::io::prelude::*;
@@ -9,7 +9,7 @@ use std::fs::File;
 use std::path::Path;
 
     /// loads the base Configuration of this Application
-pub fn database_config() -> Result<DatabaseConfig, Error>{
+pub fn database_config() -> Result<ApplicationConfig, Error>{
         
     let mut json = String::new();
     let config_path = Path::new("config.json");
@@ -21,12 +21,14 @@ pub fn database_config() -> Result<DatabaseConfig, Error>{
 
     //read from config File to the json String
     file.read_to_string(&mut json).expect("could not read from config file");
-    let config: DatabaseConfig = serde_json::from_str(&json)?;
+    let config: ApplicationConfig = serde_json::from_str(&json)?;
     Ok(config)
 }
 
 pub fn create_default_config() -> Result<(),Error>{
-    let config = DatabaseConfig::new(5432,"localhost".into(),"username".into(),None,"databaseName".into());
+    let db_config = DatabaseConfig::new(5432,"localhost".into(),"username".into(),None,"databaseName".into());
+    let web_config = WebServiceConfig::new(true,"address".into(),8080);
+    let config = ApplicationConfig::new("Telegram_Api_key".into(),true,true,web_config,db_config);
     let json = serde_json::to_string(&config)?;
     
     let config_path = Path::new("config.json");
