a4027ba61c57a547efccd95d3b591ee44c10a304
doenis <Dstu93@users.noreply.github.com>
2018-01-19 20:35:21 +0100

    added function to decode base64 Stings

--- src/recipe_manager/utils.rs
+++ src/recipe_manager/utils.rs
@@ -1,4 +1,14 @@
 
+extern crate base64;
+
+use self::base64::{decode,DecodeError};
+
+/// adds a line break to the string
 pub fn new_line(s : &mut String){
     s.push_str("\n");
+}
+
+/// encodes a given base64 string
+pub fn base64_decode(s: &String) -> Result<Vec<u8>, DecodeError> {
+    decode(s)
 }
