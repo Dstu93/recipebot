8d11434aff7b5add5cc5252c097d15ba35940036
dstu <dstu93@noreply-github.com>
2018-03-10 15:18:10 +0100

    tmp

--- src/recipe_bot/commands/details.rs
+++ src/recipe_bot/commands/details.rs
@@ -7,17 +7,18 @@ use recipe_bot::commands::error::CmdError;
 use recipe_bot::commands::command::RecipeCommand;
 
 use teleborg::{Bot,Command};
+use teleborg::objects::Update;
 
 /// Details command for the recipe-bot.
 /// takes one or more ids of recipes and returns the recipe details as string.
 pub struct DetailsCommand{
-    dao: RwLock<Box<RecipeDAO + Send>>,
+    dao: RwLock<Box<RecipeDAO + Send + Sync>>,
 }
 
 impl DetailsCommand{
 
     /// creates a new DetailsCommand-Object
-    pub fn new(dao: RwLock<Box<RecipeDAO + Send>>) -> DetailsCommand{
+    pub fn new(dao: RwLock<Box<RecipeDAO + Send + Sync>>) -> DetailsCommand{
         DetailsCommand{dao}
     }
 }
@@ -34,20 +35,30 @@ impl RecipeCommand for DetailsCommand{
 
     fn execute_cmd(&self, args: Option<Vec<&str>>) -> Result<String,CmdError>{
 
-        let ids: Vec<u32> = Vec::new();
+        if args.is_none(){
+            return Err(CmdError::NoArguments);
+        }
+
+        let mut ids: Vec<u32> = Vec::new();
+        let args = args.unwrap();
+        for arg in args {
+
+        }
+
 
         let lock = self.dao.read();
         if lock.is_err(){
             return Err(CmdError::DatabaseAccessError);
         }
+        let dao = lock.unwrap();
         let mut recipes = Vec::with_capacity(ids.len());
-        for id in ids {
-            let res = lock.unwrap().find_by_id(&id);
+        for id in &ids {
+            let res = dao.find_by_id(id);
             if res.is_ok(){ recipes.push(res.unwrap());}
             //TODO Error Message when recipe with id was not found
         }
 
-        let mut answer = String::new(); //maybe with precalculated capacity?
+        let mut answer = String::with_capacity(50 * ids.len()); //maybe with precalculated capacity?
         //TODO adding Recipes to answer
 
         Ok(answer)
@@ -60,13 +71,17 @@ impl RecipeCommand for DetailsCommand{
 }
 
 impl Command for DetailsCommand{
-    fn execute(&mut self, bot: &bot::Bot, update: Update, args: Option<Vec<&str>>){
+    fn execute(&mut self, bot: &Bot, update: Update, args: Option<Vec<&str>>){
         let res = self.execute_cmd(args);
-        let answer = match res {
-            Ok(answer) => {&*answer},
-            Err(e) => {util::translate_error(&e)}
-        };
-        bot.reply_to_message(&update,answer);
+        match res {
+            Ok(answer) => {
+                bot.reply_to_message(&update,&*answer);
+            },
+            Err(e) => {
+                let answer = util::translate_error(&e);
+                bot.reply_to_message(&update,answer);
+            }
+        }
     }
 }
 
--- src/tests/commands/details_test.rs
+++ src/tests/commands/details_test.rs
@@ -9,24 +9,24 @@ use recipe_bot::commands::details::DetailsCommand;
 
 #[test]
 fn details_command_test(){
-    let dao = RwLock::new(Box::new(RecipeDAOMock::new()));
-    let cmd = DetailsCommand::new(dao);
+    let dao = Box::new(RecipeDAOMock::new());
+    let cmd = DetailsCommand::new(RwLock::new(dao));
 
     //First we do not pass any arguments, we should get
     //an error for invalid input
     let args = None;
     let result = cmd.execute_cmd(args);
-    assert_eq!(result.unwrap(),CmdError::NoArguments);
+    assert_eq!(result.unwrap_err(),CmdError::NoArguments);
 
     //now we does the same with an empty vec
     let args = Some(Vec::new());
     let result = cmd.execute_cmd(args);
-    assert_eq!(result.unwrap(),CmdError::NoArguments);
+    assert_eq!(result.unwrap_err(),CmdError::NoArguments);
 
     //arg literal instead of number
     let args = Some(vec!["n"]);
     let result = cmd.execute_cmd(args);
-    assert_eq!(result.unwrap(),CmdError::InvalidInput);
+    assert_eq!(result.unwrap_err(),CmdError::InvalidInput);
 
     //right argument
     let args = Some(vec!["0"]);
@@ -39,5 +39,5 @@ fn details_command_test(){
     let result = cmd.execute_cmd(args);
     assert!(result.is_ok());
     //TODO validate string
-    
+
 }
